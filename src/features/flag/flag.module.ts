789554fd32d9bbd29f93280a85ba788dbc09ffbd
jackula83 <jackula@gmail.com>
2022-09-19 20:28:23 +1000

    reverted nest back to v8 due to known fastify issue in v9

--- src/features/flag/flag.module.ts
+++ src/features/flag/flag.module.ts
@@ -1,13 +1,24 @@
-import { Module } from "@nestjs/common";
+import { Module, Query } from "@nestjs/common";
+import { CqrsModule } from "@nestjs/cqrs";
 import { ConfigModule } from "@nestjs/config";
 import { FlagResolver } from './flag.resolvers';
 import { FlagService } from './flag.service';
 import { CoreModule } from '../../core/core.module';
+import { FlagToggleCommandHandler } from "./cqrs/flagToggle.handler";
+import { FlagQueryHandler } from "./cqrs/flag.handler";
+
+export const CommandHandlers = [FlagToggleCommandHandler];
+export const QueryHandlers = [FlagQueryHandler];
 
 @Module({
-  imports: [ConfigModule, CoreModule],
-  providers: [FlagResolver, FlagService],
-  exports: [FlagResolver, FlagService]
+  imports: [ConfigModule, CqrsModule, CoreModule],
+  providers: [
+    FlagResolver, 
+    FlagService,
+    ...CommandHandlers,
+    ...QueryHandlers
+  ],
+  exports: [FlagService]
 })
 
 export class FlagModule {}
